a2abec3fee1b39e5f8b4b2c8abd26117fa369e61
dmong <dt2980_hee@naver.com>
2023-11-13 03:06:36 +0900

    feat: OutputView 단순 출력 로직 작성

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,33 @@
+package christmas.view;
+
+public class OutputView {
+
+    public void printInitMessage(int date){
+        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!", 3);
+        System.out.println();
+    }
+
+    public void printOrderMenus(){
+        System.out.println("<주문 메뉴>");
+    }
+
+    public void printAmountBeforeDiscount(){
+        System.out.println("<할인 전 총주문 금액>");
+    }
+
+    public void printGiftMenu(){
+        System.out.println("<증정 메뉴>");
+    }
+
+    public void printRewards(){
+        System.out.println("<혜택 내역>");
+    }
+
+    public void printAmountOfRewards(){
+        System.out.println("<총혜택 금액>");
+    }
+
+    public void printExpectedPayAmount(){
+        System.out.println("<할인 후 예상 결제 금액>");
+    }
+}
