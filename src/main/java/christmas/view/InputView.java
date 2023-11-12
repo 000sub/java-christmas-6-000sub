13a1b11e8b5db464614cc49cbaf0d6f65b33af37
dmong <dt2980_hee@naver.com>
2023-11-13 00:59:16 +0900

    feat: UI 사용자 입력 처리 구현

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,41 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public int readDate(){
+        printDateInputMessage();
+        String userInput = readLine();
+        validateNumber(userInput);
+        return Integer.parseInt(userInput);
+    }
+
+    public String readMenu(){
+        printMenuInputMessage();
+        return readLine();
+    }
+
+    private String readLine(){
+        return Console.readLine();
+    }
+
+    public void printInitMessage(){
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    private void printDateInputMessage(){
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+    }
+
+    private void printMenuInputMessage(){
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1");
+    }
+
+    private void validateNumber(String input){
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e){
+            throw new NumberFormatException();
+        }
+    }
+}
