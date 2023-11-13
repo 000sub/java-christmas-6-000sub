4031927ad96f7e7f57a77d9dded908367810c3db
dmong <dt2980_hee@naver.com>
2023-11-13 13:10:58 +0900

    feat: DTO 변환 로직을 담당하는 DtoMapper 클래스 작성

---
+++ src/main/java/christmas/domain/dto/DtoMapper.java
@@ -0,0 +1,26 @@
+package christmas.domain.dto;
+
+import christmas.domain.Order;
+import christmas.domain.OrderedItem;
+import christmas.domain.dto.request.OrderedItemRequestDto;
+import christmas.domain.dto.response.OrderResultDto;
+import java.util.List;
+
+public class DtoMapper {
+    private DtoMapper() {
+    }
+
+    public static List<OrderedItem> convertDtosToOrderedItems(List<OrderedItemRequestDto> orderedItemRequestDtos) {
+        return orderedItemRequestDtos.stream()
+                .map(DtoMapper::convertDtoToOrderedItem)
+                .toList();
+    }
+
+    private static OrderedItem convertDtoToOrderedItem(OrderedItemRequestDto orderedItemRequestDto) {
+        return new OrderedItem(orderedItemRequestDto.getMenuName(), orderedItemRequestDto.getQuantity());
+    }
+
+    public static OrderResultDto convertOrderToDto(Order order) {
+        return new OrderResultDto(order.getOrderedItems(), order.getTotalAmount(), order.getVisitDate());
+    }
+}
