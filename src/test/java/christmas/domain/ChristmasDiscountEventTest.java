c212fcf1feed45cd383a355887a64e7d30ce6744
dmong <dt2980_hee@naver.com>
2023-11-14 13:22:12 +0900

    test: DiscountEvent 테스트 변경 사항 반영

--- src/test/java/christmas/domain/ChristmasDiscountEventTest.java
+++ src/test/java/christmas/domain/ChristmasDiscountEventTest.java
@@ -1,5 +1,6 @@
 package christmas.domain;
 
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -7,23 +8,30 @@ class ChristmasDiscountEventTest {
 
     ChristmasDiscountEvent christmasDiscountEvent = new ChristmasDiscountEvent();
 
+    private Order getTestOrder(int date) {
+        OrderedItem orderedItem = new OrderedItem("크리스마스파스타", 1);
+        List<OrderedItem> orderedItems = List.of(orderedItem);
+        return Order.of(date, orderedItems);
+    }
+
     @Test
     void 첫날_할인_금액은_1000원() {
-        int discountAmount = christmasDiscountEvent.getDiscountAmount(1);
+
+        int discountAmount = christmasDiscountEvent.getDiscountAmount(getTestOrder(1));
 
         Assertions.assertThat(discountAmount).isEqualTo(1000);
     }
 
     @Test
     void 마지막날_할인_금액은_3400원() {
-        int discountAmount = christmasDiscountEvent.getDiscountAmount(25);
+        int discountAmount = christmasDiscountEvent.getDiscountAmount(getTestOrder(25));
 
         Assertions.assertThat(discountAmount).isEqualTo(3400);
     }
 
     @Test
     void 기간외_할인_금액은_0원() {
-        int discountAmount = christmasDiscountEvent.getDiscountAmount(29);
+        int discountAmount = christmasDiscountEvent.getDiscountAmount(getTestOrder(29));
 
         Assertions.assertThat(discountAmount).isEqualTo(0);
     }
