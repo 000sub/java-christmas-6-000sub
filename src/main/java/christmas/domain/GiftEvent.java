27bd3fbf47038cc23d9bd3cc75c66bcb2c17f83f
dmong <dt2980_hee@naver.com>
2023-11-15 08:55:16 +0900

    refactor: getGift의 반환타입 Menu->List<GiftItem> 변경

--- src/main/java/christmas/domain/GiftEvent.java
+++ src/main/java/christmas/domain/GiftEvent.java
@@ -1,5 +1,9 @@
 package christmas.domain;
 
+import static christmas.domain.Menu.CHAMPAGNE;
+
+import java.util.List;
+
 public class GiftEvent extends Event {
     private static final int MINIMUM_TOTAL_AMOUNT = 120000;
 
@@ -8,7 +12,7 @@ public class GiftEvent extends Event {
         return order.getTotalAmount() > MINIMUM_TOTAL_AMOUNT;
     }
 
-    public Menu getGift() {
-        return Menu.CHAMPAGNE;
+    public List<GiftItem> getGifts() {
+        return List.of(GiftItem.of(CHAMPAGNE, 1));
     }
 }
