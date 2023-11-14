c0c7e2c823d2e57b236b0f1f0f30598c776b6429
dmong <dt2980_hee@naver.com>
2023-11-14 14:25:08 +0900

    feat: 평일 할인 적용 여부 판별 로직 추가

--- src/main/java/christmas/domain/WeekdayDiscountEvent.java
+++ src/main/java/christmas/domain/WeekdayDiscountEvent.java
@@ -2,6 +2,8 @@ package christmas.domain;
 
 import static christmas.domain.MenuCategory.DESSERT;
 
+import java.time.DayOfWeek;
+
 public class WeekdayDiscountEvent extends DiscountEvent {
     private static final int UNIT_DISCOUNT_AMOUNT = 2023;
 
@@ -13,6 +15,10 @@ public class WeekdayDiscountEvent extends DiscountEvent {
 
     @Override
     public boolean canApply(Order order) {
-        return false;
+        return isWeekday(order.getVisitDate().getDayOfWeek());
+    }
+
+    private boolean isWeekday(DayOfWeek dayOfWeek) {
+        return dayOfWeek != DayOfWeek.FRIDAY && dayOfWeek != DayOfWeek.SATURDAY;
     }
 }
