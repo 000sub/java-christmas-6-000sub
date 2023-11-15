f87755639c3e8c00b75919faa3fbbe50506ddee6
dmong <dt2980_hee@naver.com>
2023-11-15 10:51:26 +0900

    refactor: 특별 할인의 상수명을 더 명확하게 수정

--- src/main/java/christmas/domain/SpecialDiscountEvent.java
+++ src/main/java/christmas/domain/SpecialDiscountEvent.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 public class SpecialDiscountEvent extends DiscountEvent {
     private static final List<Integer> SPECIAL_DAYS = List.of(3, 10, 17, 24, 25, 31);
-    private static final int DISCOUNT_AMOUNT = 1000;
+    private static final int FIXED_DISCOUNT_AMOUNT = 1000;
 
     @Override
     protected int calculateDiscountAmount(Order order) {
-        return DISCOUNT_AMOUNT;
+        return FIXED_DISCOUNT_AMOUNT;
     }
 
     @Override
