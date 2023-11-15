5c178ad223ae3d16fde9ce706173e56bef8a2665
dmong <dt2980_hee@naver.com>
2023-11-15 13:12:06 +0900

    refactor: ChristmasDiscountEvent에서 LocalDate를 처리하도록 변경

--- src/main/java/christmas/domain/ChristmasDiscountEvent.java
+++ src/main/java/christmas/domain/ChristmasDiscountEvent.java
@@ -1,24 +1,30 @@
 package christmas.domain;
 
+import static christmas.domain.Constants.CURRENT_MONTH;
+import static christmas.domain.Constants.CURRENT_YEAR;
+import static java.time.temporal.ChronoUnit.DAYS;
+
+import java.time.LocalDate;
+
 public class ChristmasDiscountEvent extends DiscountEvent {
-    private static final int START_DATE = 1;
-    private static final int END_DATE = 25;
+    private static final LocalDate START_DATE = LocalDate.of(CURRENT_YEAR.getValue(), CURRENT_MONTH.getValue(), 1);
+    private static final LocalDate END_DATE = LocalDate.of(CURRENT_YEAR.getValue(), CURRENT_MONTH.getValue(), 25);
     private static final int INIT_AMOUNT = 1000;
     private static final int ACCUMULATED_DISCOUNT_AMOUNT = 100;
 
     @Override
     protected int calculateDiscountAmount(Order order) {
-        int date = order.getVisitDate().getDayOfMonth();
-        return INIT_AMOUNT + (date - START_DATE) * ACCUMULATED_DISCOUNT_AMOUNT;
+        LocalDate date = order.getVisitDate();
+        return INIT_AMOUNT + (int) (DAYS.between(START_DATE, date)) * ACCUMULATED_DISCOUNT_AMOUNT;
     }
 
     @Override
     protected boolean isEligible(Order order) {
-        return checkEligibleDate(order.getVisitDate().getDayOfMonth());
+        return isWithinDateRange(order.getVisitDate());
     }
 
-    private boolean checkEligibleDate(int date) {
-        return date >= START_DATE && date <= END_DATE;
+    private boolean isWithinDateRange(LocalDate date) {
+        return !date.isBefore(START_DATE) && !date.isAfter(END_DATE);
     }
 
 
